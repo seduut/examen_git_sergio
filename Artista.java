10025dc4abc6f6c7df5955ffa0a3b60d57373dd9
seduut <sergio.duran.utrera.alu@iesfernandoaguilar.es>
2024-03-14 16:53:48 +0100

    cambios a Artista en la rama master de la carpeta examen_git_sergio

--- Artista.java
+++ Artista.java
@@ -18,7 +18,7 @@ public class Artista {
         return nombreArtista;
 	//Comentario del apartado B
 	//Esto es un comentario
-	//Esto es otro comentario
+	//He cambiado este comentario, estoy en la rama master del otro
     }
     
 }
